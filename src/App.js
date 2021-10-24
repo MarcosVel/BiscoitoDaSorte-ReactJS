0fd3f6a6f6f11d473f8c988b47d6a79e54376d94
MarcosVel <marcos.veloso@sempreceub.com>
2021-10-24 15:53:29 -0300

    mostrando frases

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      textoFrase: ''
+    };
+
+    this.quebrarBiscoito = this.quebrarBiscoito.bind(this);
 
     this.frases = [
       "A vida trará coisas boas se tiveres paciência.",
@@ -75,12 +79,21 @@ class App extends Component {
     ]
   }
 
+  quebrarBiscoito() {
+    let state = this.state;
+
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+
+    state.textoFrase = `"${ this.frases[ numeroAleatorio ] }"`;
+    this.setState(state);
+  }
+
   render() {
     return (
       <div className="container">
         <img src={ BiscoitoImg } className="img" />
-        <Botao />
-        <h3 className="textoFrase">Frase numero 1</h3>
+        <Botao nome='Abrir Biscoito' acaoBtn={ this.quebrarBiscoito } />
+        <h3 className="textoFrase">{ this.state.textoFrase }</h3>
       </div>
     );
   }
@@ -90,7 +103,7 @@ class Botao extends Component {
   render() {
     return (
       <div>
-        <button>Abrir Biscoito</button>
+        <button onClick={ this.props.acaoBtn }>{ this.props.nome }</button>
       </div>
     )
   }
